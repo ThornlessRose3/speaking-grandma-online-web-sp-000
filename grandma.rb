648789d50b067498152377f816f22b4f6491e49b
Carrie Jones <c.jessica.jones@gmail.com>
2020-05-31 01:56:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(shout)
-  shout = phrase
+  phrase = shout
   if phrase == "I LOVE YOU GRANDMA!"
     puts 'I LOVE YOU TOO PUMPKIN!'
   elsif phrase == phrase.upcase
